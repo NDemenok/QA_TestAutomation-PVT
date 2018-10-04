a02e5d4adfb57f223a052ed5b1359479856e6e93
email <NDemenok@gmail.com>
2018-10-04 17:16:13 +0300

    HomeTask1

---
+++ QA_TestAutomation-PVT/HomeTask1/imp(Task6).java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class imp {
+
+	public static void main(String[] args) 
+	{
+		Random generator = new Random();
+		int rnd = generator.nextInt(1000000);
+		System.out.println("Random number = " + rnd);
+	}
+
+}
