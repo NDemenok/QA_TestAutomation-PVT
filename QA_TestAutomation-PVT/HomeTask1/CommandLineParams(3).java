81ad1e7706544086429c3aa445df88da1f33c279
email <NDemenok@gmail.com>
2018-10-04 17:16:13 +0300

    HomeTask1

---
+++ QA_TestAutomation-PVT/HomeTask1/CommandLineParams(3).java
@@ -0,0 +1,11 @@
+
+public class CommandLineParams_2 {
+
+	public static void main(String[] args) {
+		
+		for (int i=2; i>=0; i--) {
+			System.out.println("Аргумент"+"i"+"="+args[i]);
+		}
+	}
+
+}
---
+++ QA_TestAutomation-PVT/HomeTask1/HelloNatali (Task4).java
@@ -0,0 +1,12 @@
+
+
+public class HelloNatali {
+
+	public static void main(String args []) {
+		
+		String f;
+		f = "Natali";
+		
+		System.out.println("Hello,"+ f +"!!!!!!");
+	}
+}
